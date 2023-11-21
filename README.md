71131f72170e7d49de08a026ca3960e11e1ecde1
rscriptsfivem <151592687+rscriptsfivem@users.noreply.github.com>
2023-11-20 22:55:57 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rScripts
-Fivem Scripts
+# Setup
+It's pretty simple, once you drop the nh-context resource into your resources folder just make sure you put
+
+ensure nh-context
+
+in your server.cfg. 
+
+
+# Credits
+(https://github.com/nerohiro/nh-context/issues)
+
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+game "gta5"
+
+name "NUI Context Menu"
+description "A modular context Menu for FiveM"
+author "NeroHiro"
+
+ui_page "./ui/index.html"
+
+files{
+    "./ui/index.html",
+    "./ui/main.css",
+    "./ui/main.js",
+	'./ui/bankgothic.ttf',
+}
+
+client_script "client.lua"
